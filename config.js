0d0aea8bbde10ec7136fec77e2935e3778bde054
JyWangT <77719225+JyWangT@users.noreply.github.com>
2026-09-17 15:25:40 -0700

    Update API_URL to new deployment URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ★ 只需要改這一行：Apps Script 部署後的 /exec 網址
-window.API_URL = 'https://script.google.com/macros/s/AKfycbxBYC7W1xxeFvb62Zyb0fvViekUNs5QK9Mc96GMSfmFLyPmijNpBw6z1V7axL_4M70nIw/exec';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbwkIKZ08nUDPPIHBdi69EZgqub4N4wDvnxtxArMQ2bMt3d95rfAAzzbxdet41E_AUR0xw/exec';
  
